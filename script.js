79bebc93bc28af1bcf95d4caaaaa2d00a02d9fb8
Habib826 <habib826826@.com>
2021-02-07 21:10:51 +0600

    check

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function search() {
-    const input = document.getElementById("input-value").innerText;
-    console.log(input);
+    const input = document.getElementById("input-value").value;
+
+    if (input == ' ') {
+        alert("give a name first")
+    }
     const mealName = input;
-    console.log(mealName);
-    
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
         .then(res => res.json())
         .then(data => displayMeals(data));
@@ -12,7 +13,7 @@ function search() {
         mealList.meals.forEach(meal => {
             const mainDiv = document.createElement('div');
             mainDiv.className = 'main-div';
-        const mealInfo = `
+            const mealInfo = `
         <P>
         <div id="first-div" onclick="mealDetail('${meal.strMeal}')">
         <img src="${meal.strMealThumb}">
@@ -22,42 +23,42 @@ function search() {
          `;
             mainDiv.innerHTML = mealInfo;
             div.appendChild(mainDiv);
-        });       
-        
+        });
+
     }
 
-     mealDetail= meal=>{
-        const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+    mealDetail = meal => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
         fetch(url)
-        .then(res=> res.json())
-        .then(data=> selectMeal(data.meals[0]))
+            .then(res => res.json())
+            .then(data => selectMeal(data.meals[0]))
     }
-    const selectMeal= meals=>{
+    const selectMeal = meals => {
         console.log(meals);
-        const mealDiv= document.getElementById("details");
-        mealDiv.innerHTML=`
+        const mealDiv = document.getElementById("details");
+        mealDiv.innerHTML = `
         <div class="Ingredient">
         <img src="${meals.strMealThumb}">
           <h4 class="meal-name">${meals.strMeal}</h4>
           <h5 class="meal-Ingredient">Ingredient:</h5>
           <ul>
-          <li>${meals.strMeasure1}${meals.str1}</li>
-          <li>${meals.strMeasure2}${meals.strIngredient2}</li>
-          <li>${meals.strMeasure3}${meals.strIngredient3}</li>
-          <li>${meals.strMeasure4}${meals.strIngredient4}</li>
-          <li>${meals.strMeasure5}${meals.strIngredient5}</li>
-          <li>${meals.strMeasure6}${meals.strIngredient6}</li>
-          <li>${meals.strMeasure7}${meals.strIngredient7}</li>
-          <li>${meals.strMeasure8}${meals.strIngredient8}</li>
-          <li>${meals.strMeasure9}${meals.strIngredient9}</li>
-          <li>${meals.strMeasure10}${meals.strIngredient10}</li> 
+          <li>${meals.strMeasure1} ${meals.strIngredient1}</li>
+          <li>${meals.strMeasure2} ${meals.strIngredient2}</li>
+          <li>${meals.strMeasure3} ${meals.strIngredient3}</li>
+          <li>${meals.strMeasure4} ${meals.strIngredient4}</li>
+          <li>${meals.strMeasure5} ${meals.strIngredient5}</li>
+          <li>${meals.strMeasure6} ${meals.strIngredient6}</li>
+          <li>${meals.strMeasure7} ${meals.strIngredient7}</li>
+          <li>${meals.strMeasure8} ${meals.strIngredient8}</li>
+          <li>${meals.strMeasure9} ${meals.strIngredient9}</li>
+          <li>${meals.strMeasure10} ${meals.strIngredient10}</li> 
           </ul>
           </div>
 
         `
 
     }
-     
+
 }
 
 
